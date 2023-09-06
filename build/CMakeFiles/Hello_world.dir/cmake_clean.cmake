94ffd1faf5dbb5c7e0068ea93a8f59606bf0b889
Marsel <marslanov481@gmail.com>
2023-09-06 14:56:27 +0300

    Init commit

---
+++ build/CMakeFiles/Hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_world.dir/main.c.o"
+  "CMakeFiles/Hello_world.dir/main.c.o.d"
+  "Hello_world"
+  "Hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
